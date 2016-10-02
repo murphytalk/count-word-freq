0f717608eebe23c266a773d85b75ec339c3f514e
Mu Lu <murphytalk@gmail.com>
2016-10-02 21:46:48 +0900

    init

--- src/views.py
+++ src/views.py
@@ -2,12 +2,13 @@
 Routes and views for the flask application.
 """
 from flask import render_template, g, current_app, Response, url_for
+from app import app
+
 
 @app.route('/')
-def summary():
-    """Renders the home page."""
-    current_app.logger.debug('idx=%s', url_for("summary"))
+def parseFile():
+    current_app.logger.debug('idx=%s', url_for("parseFile"))
     return render_template(
         'index.html',
-        title='Home Page'
+        title='Parse File'
     )
